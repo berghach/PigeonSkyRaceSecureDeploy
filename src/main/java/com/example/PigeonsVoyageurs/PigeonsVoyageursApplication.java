3c8aa2631b14766479ab1ffe31fc810f9fa18c5b
berghach <berghach.chemseddine@gmail.com>
2024-12-03 12:41:47 +0100

    PIGV-4 the project dependencies are reset

--- src/main/java/com/example/PigeonsVoyageurs/PigeonsVoyageursApplication.java
+++ src/main/java/com/example/PigeonsVoyageurs/PigeonsVoyageursApplication.java
@@ -19,40 +19,4 @@ public class PigeonsVoyageursApplication {
 
 		SpringApplication.run(PigeonsVoyageursApplication.class, args);
 	}
-
-
-
-	@Configuration
-	public class CompetitionDataLoader {
-
-//		@Bean
-//		public CommandLineRunner loadCompetitions(CompetitionRepository competitionRepository) {
-//			return args -> {
-//				Competition competition1 = new Competition();
-//				competition1.setRaceName("Course de Printemps");
-//				competition1.setReleasePoint(new Coordinate(34.0522, -118.2437)); // Exemple de coordonnées
-//				competition1.setStart(new Date());
-//				competition1.setPreviewedDistance(500.0);
-//
-//				Competition competition2 = new Competition();
-//				competition2.setRaceName("Course d'Été");
-//				competition2.setReleasePoint(new Coordinate(40.7128, -74.0060)); // Exemple de coordonnées
-//				competition2.setStart(new Date());
-//				competition2.setPreviewedDistance(700.0);
-//
-//				Competition competition3 = new Competition();
-//				competition3.setRaceName("Course d'Automne");
-//				competition3.setReleasePoint(new Coordinate(51.5074, -0.1278)); // Exemple de coordonnées
-//				competition3.setStart(new Date());
-//				competition3.setPreviewedDistance(600.0);
-//
-//				// Sauvegarder les compétitions dans le repository
-//				competitionRepository.saveAll(Arrays.asList(competition1, competition2, competition3));
-//
-//				System.out.println("Compétitions initiales insérées dans la base de données.");
-//			};
-//		}
-	}
-
-
 }
